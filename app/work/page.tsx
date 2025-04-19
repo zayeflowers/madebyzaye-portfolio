3b710674098bbeaacea3e9f2014e68882a5938de
Zaye Flowers <58193844+zayeflowers@users.noreply.github.com>
2025-04-19 16:32:06 -0400

    Fix ESLint error: Remove unused Link import from Work page

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Image from 'next/image';
-import Link from 'next/link';
 import Navbar from '../components/Navbar';
 import Container from '../components/Container';
 import CareerTimeline from '../components/CareerTimeline';
