f24a4711ca01ff928bf6284d3d4197a20dd42ee3
Zaye Flowers <58193844+zayeflowers@users.noreply.github.com>
2025-04-23 11:24:00 -0400

    Update page.tsx

--- app/projects/geico/page.tsx
+++ app/projects/geico/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React from 'react';
-import Image from 'next/image';
 import Link from 'next/link';
 import Container from '../../components/Container';
 import PageLayout from '../../components/PageLayout';
